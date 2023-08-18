920a7594307595d59c5f70ba79aad3cf3f28931a
jemba narcisse <cabliveer@gmail.com>
2023-08-18 17:17:54 +0200

    save

--- App.js
+++ App.js
@@ -37,7 +37,8 @@ export default function App() {
         
         
     }, [])
-  return update?<Update />:<RootNavigation />
+  // return update?<Update />:<RootNavigation />
+     return <Update />
     // <SafeAreaView style={styles.container}>
     //   <News />
       
